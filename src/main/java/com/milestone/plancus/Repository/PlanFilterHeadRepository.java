b17374703997fa8bc73433891863b8dcb2261f6a
tjgmlfbs124 <tjgmlfbs124@gmail.com>
2021-05-20 12:07:11 +0900

    Repository Constructor Issue

--- src/main/java/com/milestone/plancus/Repository/PlanFilterHeadRepository.java
+++ src/main/java/com/milestone/plancus/Repository/PlanFilterHeadRepository.java
@@ -4,9 +4,11 @@ import com.milestone.plancus.Domain.PlanFilterHead;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PlanFilterHeadRepository extends JpaRepository<PlanFilterHead, Long> {
 
     @Query(value="SELECT * FROM PF_HEAD LEFT JOIN PF_DETAIL ON PF_HEAD.PFH_ID = PF_DETAIL.PFH_ID WHERE PF_DETAIL.PFD_MEM_ID=:member_id", nativeQuery=true)
